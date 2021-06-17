f5f23fd4fc92105b303774702fabe2998973e8ef
Victor Wolff <victorwolff39@gmail.com>
2021-06-17 20:25:53 -0300

    Initial commit

---
+++ src/test/java/net/alerok/listacontatosrest/ListaContatosRestApplicationTests.java
@@ -0,0 +1,13 @@
+package net.alerok.listacontatosrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaContatosRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
