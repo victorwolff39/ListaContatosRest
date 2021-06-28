d7e2b8808f9256119371be2df8f37e0e5dc0d088
Victor Wolff <victorwolff39@gmail.com>
2021-06-28 00:17:34 -0300

    Inseridas anotações @Api, @CrossOrigin e @ApiOperation's nos controllers.

--- src/main/java/net/alerok/listacontatosrest/api/controller/StatusController.java
+++ src/main/java/net/alerok/listacontatosrest/api/controller/StatusController.java
@@ -1,16 +1,21 @@
 package net.alerok.listacontatosrest.api.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import net.alerok.listacontatosrest.domain.service.StatusService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/api/status")
+@Api(value = "REST API Test")
+@CrossOrigin(origins = "*")
 public class StatusController {
 
-    private StatusService statusService;
+    private final StatusService statusService;
 
     @Autowired
     public StatusController(StatusService statusService) {
@@ -18,6 +23,7 @@ public class StatusController {
     }
 
     @GetMapping
+    @ApiOperation(value = "Checks if the API is running.")
     public String check() {
         return statusService.check();
     }
