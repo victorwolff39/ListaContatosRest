e13f9b6b7d98ec9b7c1c153611cbd6b5f8a0beab
Victor Wolff <victorwolff39@gmail.com>
2021-07-04 22:49:57 -0300

    Arrumando bug que usuários não podiam se cadastrar.

--- src/main/java/net/alerok/listacontatosrest/api/auth/SecurityConfiguration.java
+++ src/main/java/net/alerok/listacontatosrest/api/auth/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import net.alerok.listacontatosrest.api.filter.JwtRequestFilter;
 import net.alerok.listacontatosrest.domain.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -37,10 +38,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests()
-                .antMatchers("/api/users/**").hasAuthority("ADMIN")
+                .antMatchers(HttpMethod.POST, "/api/users").permitAll()
                 .antMatchers("/api/authenticate").permitAll()
-                .antMatchers("/api/status").permitAll()
-                .antMatchers("/**").permitAll()
                 .anyRequest().authenticated()
                 .and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
