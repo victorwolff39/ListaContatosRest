4ecce69646f0aa32cd179be777998104957fb266
Victor Wolff <victorwolff39@gmail.com>
2021-07-05 17:47:13 -0300

    Retornando o id na autenticacao

--- src/main/java/net/alerok/listacontatosrest/domain/model/AuthenticationResponse.java
+++ src/main/java/net/alerok/listacontatosrest/domain/model/AuthenticationResponse.java
@@ -4,11 +4,18 @@ public class AuthenticationResponse {
 
     private final String jwt;
 
-    public AuthenticationResponse(String jwt) {
+    private final Long userId;
+
+    public AuthenticationResponse(String jwt, Long userId) {
         this.jwt = jwt;
+        this.userId = userId;
     }
 
     public String getJwt() {
         return jwt;
+    }
+
+    public Long getUserId() {
+        return userId;
     }
 }
