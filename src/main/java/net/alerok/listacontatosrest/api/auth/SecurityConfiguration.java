db1c6a73dc1943536b68ca9e44955763fbbfd539
Victor Wolff <victorwolff39@gmail.com>
2021-07-07 00:04:20 -0300

    Implementando o BCryptPasswordEncoder na SecutiryConfiguration.

--- src/main/java/net/alerok/listacontatosrest/api/auth/SecurityConfiguration.java
+++ src/main/java/net/alerok/listacontatosrest/api/auth/SecurityConfiguration.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -46,9 +47,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
     }
 
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        //Todo: Implement password encoding
+//        return NoOpPasswordEncoder.getInstance();
+//    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
-        //Todo: Implement password encoding
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 }
